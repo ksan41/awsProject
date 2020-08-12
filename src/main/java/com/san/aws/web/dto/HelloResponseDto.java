601daf4ac3ef67c8cb57c7aeb9f72e389ccbfe12
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-12 16:00:42 +0900

    Update HelloResponseDto.java

--- src/main/java/com/san/aws/web/dto/HelloResponseDto.java
+++ src/main/java/com/san/aws/web/dto/HelloResponseDto.java
@@ -1,4 +1,12 @@
 package com.san.aws.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor // 선언된 모든 final필드가 포함된 생성자를 생성해준다.(final이 없는 필드는 포함하지 않음)
 public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
 }
