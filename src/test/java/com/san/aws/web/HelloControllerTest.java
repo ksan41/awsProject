ffeba8df1ea5fdd9c0f93936793de0ec025d6323
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-12 16:18:58 +0900

    Update HelloControllerTest.java

--- src/test/java/com/san/aws/web/HelloControllerTest.java
+++ src/test/java/com/san/aws/web/HelloControllerTest.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static  org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자(SpringRunner)를 실행시킨다.
                              // => 스프링부트 테스트와 JUnit 사이에 연결자 역할을 함.
@@ -32,5 +33,23 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name="hello";
+        int amount=1000;
+
+        // .param() : API 테스트 시 사용될 요청 파라미터를 설정.
+        //            단, String 값만 허용된다.(다른 데이터 등록 시 String 변환해야 함)
+        mvc.perform(
+                get("/hello/dto")
+                .param("name",name)
+                .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+        // jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드
+        //            $을 기준으로 필드명을 명시한다.
     }
 }
