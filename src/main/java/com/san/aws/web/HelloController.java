c83e13d43c55fbd5f55b7ef66a9c142065e28440
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-12 16:03:31 +0900

    Update HelloController.java
    Add method

--- src/main/java/com/san/aws/web/HelloController.java
+++ src/main/java/com/san/aws/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.san.aws.web;
 
+import com.san.aws.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다.
@@ -13,5 +15,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        // @RequestParam : 외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션.
+        //                 'name'과 'amount'란 이름으로 넘긴 파라미터를 메소드 파라미터에 저장하게 됨.
+        return new HelloResponseDto(name,amount);
     }
 }
