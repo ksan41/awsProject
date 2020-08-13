6d521b4859a8004ef48158c6fc1e1104184390dd
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-13 18:31:50 +0900

    Create PostsUpdateDto.java

---
+++ src/main/java/com/san/aws/web/dto/PostsUpdateDto.java
@@ -0,0 +1,18 @@
+package com.san.aws.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateDto(String title, String content){
+        this.title=title;
+        this.content=content;
+    }
+}
