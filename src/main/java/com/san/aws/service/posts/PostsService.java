bb3d7099cc4750b3faa45c5ceeadd652cf8f53e5
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-13 18:31:29 +0900

    Update PostsService.java

--- src/main/java/com/san/aws/service/posts/PostsService.java
+++ src/main/java/com/san/aws/service/posts/PostsService.java
@@ -1,7 +1,11 @@
 package com.san.aws.service.posts;
 
+import com.san.aws.domain.posts.Posts;
 import com.san.aws.domain.posts.PostsRepository;
+import com.san.aws.web.dto.PostsResponseDto;
 import com.san.aws.web.dto.PostsSaveRequestDto;
+import com.san.aws.web.dto.PostsUpdateDto;
+import com.san.aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +20,26 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
+
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
