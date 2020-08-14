7df031444e9da6028e45196e816b81bfd94414df
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-14 13:54:34 +0900

    Update PostsService.java
    전체조회용 메소드 추가

--- src/main/java/com/san/aws/service/posts/PostsService.java
+++ src/main/java/com/san/aws/service/posts/PostsService.java
@@ -2,14 +2,17 @@ package com.san.aws.service.posts;
 
 import com.san.aws.domain.posts.Posts;
 import com.san.aws.domain.posts.PostsRepository;
+import com.san.aws.web.dto.PostsListResponseDto;
 import com.san.aws.web.dto.PostsResponseDto;
 import com.san.aws.web.dto.PostsSaveRequestDto;
-import com.san.aws.web.dto.PostsUpdateDto;
 import com.san.aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // @RequiredArgsConstructor : final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성해줌.(의존성 주입)
 @RequiredArgsConstructor
@@ -41,5 +44,15 @@ public class PostsService {
                         IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly=true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        // .map(PostsListResponseDto::new)
+        // ==>  .map(posts -> new PostsListResponseDdto(posts))
+        // postsRepository의 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto로 변환, -> List로 반환하는 메소드.
     }
 }
