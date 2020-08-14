b45ad37ee8f9e6947b4be0d8a1047166be326193
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-14 09:46:51 +0900

    Update Posts.java
    extends BaseTimeEntity

--- src/main/java/com/san/aws/domain/posts/Posts.java
+++ src/main/java/com/san/aws/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.san.aws.domain.posts;
 
+import com.san.aws.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,7 @@ import javax.persistence.Id;
 // Entity : 테이블과 링크될 클래스임을 나타냄.
 //          기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭.
 //          ex)SalesManager.java => sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     // Id : 해당 테이블의 PK
     @Id
