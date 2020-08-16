610d3cd430968f8dfaf72284674933dbafffed5a
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-08-17 01:44:20 +0900

    Update IndexController.java
    @LoginUser 어노테이션 활용해 코드 개선

--- src/main/java/com/san/aws/web/IndexController.java
+++ src/main/java/com/san/aws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.san.aws.web;
 
+import com.san.aws.config.auth.LoginUser;
 import com.san.aws.config.auth.dto.SessionUser;
 import com.san.aws.service.posts.PostsService;
 import com.san.aws.web.dto.PostsResponseDto;
@@ -19,11 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts",postsService.findAllDesc());
-
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName",user.getName());
